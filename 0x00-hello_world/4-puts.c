8b6b043fb9a943adaf6a8dfd0af6a7774359719f
mohap710 <mohap710@gmail.com>
2022-10-13 13:18:38 -0700

    add betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- * This program to use puts to print string to stdout stream
- * 
- */
+/**
+* main - This program to use puts to print string to stdout stream
+* Return:0
+*/
 int main(void)
 {
- puts("Programming is like building a multilingual puzzle");
- return (0);
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
