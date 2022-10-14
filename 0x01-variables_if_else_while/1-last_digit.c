74534cbe52af8b7c4c99d9519b4c78f42db3285e
mohap710 <mohap710@gmail.com>
2022-10-14 11:30:06 -0700

    betty style applied !

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,20 +12,20 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if (n % 10 == 0)
+	int last = n % 10;
+
+	if (last == 0)
 	{
-		printf("Last digit of %d is and is 0\n",n);
+		printf("Last digit of %d is %d and is 0\n", n, last);
 	}
 	else if (n % 10 < 6)
 	{
-		printf("Last digit of %d is and is less than 6 and not 0\n",n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n"
+		, n, last);
 	}
 	else
 	{
-		printf("Last digit of %d is and is greater than 5\n",n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	}
-	
-	
 	return (0);
 }
