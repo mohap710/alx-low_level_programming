8721ebb10321ffe21f6f224c04866924f0871f17
mohap710 <mohap710@gmail.com>
2022-10-24 12:46:39 -0700

    a function that prints a string, in reverse, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+* print_rev - print a string to stdout.
+* @s: pointer to array
+* Return:
+*/
+void print_rev(char *s)
+{
+	int len = _strlen(s);
+	
+	for (len--; len >= 0; len--)
+		_putchar(s[len]);
+
+	_putchar('\n');
+}
