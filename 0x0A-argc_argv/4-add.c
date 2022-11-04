bdebdee5ca508851a6b0de5fae722a2d8cd6a416
mohap710 <mohap710@gmail.com>
2022-11-04 08:27:16 -0700

    add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main -add positive numbers passed in args
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0 for success 1 for failure
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int result = 0, i = 1;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (i < argc)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result += atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", result);
+	return (0);
+}
