7f6b45e9f40992669603a7a628a62210a6986810
mohap710 <mohap710@gmail.com>
2022-11-18 21:35:48 -0800

    fixed bug

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	if (n == 0)
+	{
+		printf("\n");
 		return;
+	}
 /* Initializing argument to the list pointer */
 	va_start(ptr, n);
 	for (i = 0; i < n; i++)
