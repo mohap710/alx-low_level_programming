9794e7fdac784ded9948dc7ee06532080feafd27
mohap710 <mohap710@gmail.com>
2022-11-30 03:25:19 -0800

    how to print linked list items

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,21 @@
 #include "lists.h"
 
 /**
- * print_listint - Prints all of the elements of a linked list.
- * @h: Pointer to a linked list head.
- * Return: The number of total nodes in the linked list.
+ * print_listint - prints all the elements of a linked list
+ * @h: head of the list
+ *
+ * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	register int i = 0;
+const listint_t *cursor = h;
+size_t count = 0;
 
-	if (h == NULL)
-		return (0);
-
-	while (h)
-	{
-		printf("%i\n", h->n);
-		i++;
-		h = h->next;
-	}
-	return (i);
+while (cursor != NULL)
+{
+printf("%d\n", cursor->n);
+count += 1;
+cursor = cursor->next;
+}
+return (count);
 }
