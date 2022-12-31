c2a932f07aa2e8841bc6d8ef472c3abdb91e2d7c
mohap710 <mohap710@gmail.com>
2022-12-31 12:04:08 +0200

    Done/

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,48 +1,30 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
+#include<stdarg.h>
 #include <stddef.h>
-/**
-* _puts - print a string to stdout.
-* @str: pointer to array
-* Return:
-*/
-void _puts(const char *str)
-{
-	int c;
-
-	for (c = 0; str[c] != '\0'; c++)
-	{
-		_putchar(str[c]+'0');
-	}
-	_putchar('\n');
-}
 
 /**
-* print_strings - print provided numbers.
-* @n: numbers of argument
-* @separator: string to be printed between strings.
-* Return: nothing;
-*/
+ * print_strings - Print numbers using a separator
+ * @separator: The string separator
+ * @n: The quantity of numbers
+ * @...: The list of numbers
+ *
+ * Return: Nothing
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list ptr;
-	unsigned int i;
-	char *word;	
+va_list valist;
+unsigned int i;
+char *p;
 
-	if (n == 0)
-		return;
-/* Initializing argument to the list pointer */
-	va_start(ptr, n);
-	for (i = 0; i < n; i++)
-	{
-		word = va_arg(ptr, char *);
-		if (word == NULL)
-			word = "(nil)";
+va_start(valist, n);
 
-		_puts(word);
-		if (separator != NULL)
-			_puts(separator);
-	}
-	va_end(ptr);
-	_putchar('\n');
+for (i = 0; i < n; i++)
+{
+if (separator != NULL && i != 0)
+printf("%s", separator);
+p = va_arg(valist, char *);
+printf("%s", (p == NULL) ? "(nil)" : p);
+}
+printf("\n");
+va_end(valist);
 }
